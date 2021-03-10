8121e8798a98ca19cc1d19157f014b5509f9eb6a
zeamaria <lingardzea@gmail.com>
2021-03-10 05:28:13 +0000

    packing-list.js

--- packing-list.js
+++ packing-list.js
@@ -8,8 +8,15 @@ console.log(packingList[2]);
 
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
-console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+//console.log("Kitchen stuff to pack:");
+//for (let i = 0; i < packingList.length; i++) {
+//  console.log(packingList[i]);
+//}
+
+console.log("kicthen stuff to pack:");
+let i = 0;
+while (i< packingList.length) {
   console.log(packingList[i]);
+  i++
 }
 
